b8e7c6ac968c85dee892677e0b16cc381b5f84ef
is0urce <azathoth@mail.ru>
2016-11-05 19:58:43 +0300

    d.i.o.r.a.m.a

--- press-x-lat/src/px/common/pool.hpp
+++ press-x-lat/src/px/common/pool.hpp
@@ -1,6 +1,6 @@
 // name: pool.hpp
 // type: c++ header
-// desc: memory pool with templated specialization
+// desc: memory pool template
 // auth: is0urce
 
 #pragma once
@@ -21,10 +21,9 @@ namespace px
 		{
 			T element;
 			record* next_free;
-			record* prev_free;
 			record* next_live;
 			record* prev_live;
-			bool live;
+			bool live; // auxiliary state to avoid double released
 		};
 	public:
 		size_t size() const noexcept
@@ -35,45 +34,50 @@ namespace px
 		{
 			return m_current == Size;
 		}
+		bool empty() const noexcept
+		{
+			return m_current == 0;
+		}
+
+		// returns nullptr if all object in pool were requested, full() returns true
 		T* request() noexcept
 		{
 			record* rec = m_free;
-			if (rec == nullptr) return nullptr;
-
-			// update free ranges
-			m_free = m_free->next_free;
-
-			// update links
-			if (rec->prev_live)
+			if (rec != nullptr)
 			{
-				rec->prev_live->next_live = rec;
-			}
-			else // start or continue living queue
-			{
-				if (m_live != nullptr)
+				// update free ranges
+				m_free = m_free->next_free;
+
+				// update links
+				if (rec->prev_live)
 				{
-					m_live->prev_live = rec;
-					rec->next_live = m_live;
+					rec->prev_live->next_live = rec;
 				}
-				m_live = rec;
-			}
-			if (rec->next_live)
-			{
-				rec->next_live->prev_live = rec;
-			}
-			if (rec->prev_free)
-			{
-				rec->prev_free->next_free = rec->next_free;
-			}
-			if (rec->next_free)
-			{
-				rec->next_free->prev_free = rec->prev_free;
-			}
+				else // start or continue living queue
+				{
+					if (m_live != nullptr)
+					{
+						m_live->prev_live = rec;
+						rec->next_live = m_live;
+					}
+					m_live = rec;
+				}
+				if (rec->next_live)
+				{
+					rec->next_live->prev_live = rec;
+				}
 
-			rec->live = true;
-			++m_current;
-			return &(rec->element);
+				// modify aux fields
+				rec->live = true;
+				++m_current;
+
+				return &(rec->element);
+			}
+			return nullptr;
 		}
+
+		// pointer must be in correct range of pool
+		// it's safe to release already released objects
 		void release(T* ptr) noexcept
 		{
 			record* rec = &m_pool[(ptr - &m_pool[0].element) / sizeof(record)];
@@ -82,10 +86,27 @@ namespace px
 			{
 				// wip
 
+				// update links
+				if (rec->prev_live)
+				{
+					rec->prev_live->next_live = rec->next_live;
+				}
+				if (rec->next_live)
+				{
+					rec->next_live->prev_live = rec->prev_live;
+				}
+
+				// update roots
+				rec->next_free = m_free;
+				m_free = rec;
+				m_live = m_live == rec ? rec->next_live : m_live;
+
+				// modify aux fields
 				rec->live = false;
 				--m_current;
 			}
 		}
+
 		template <typename Operator>
 		void enumerate_active(Operator && op)
 		{
@@ -94,6 +115,7 @@ namespace px
 				std::forward<Operator>(op)(i->element);
 			}
 		}
+
 		void clear() noexcept
 		{
 			m_current = 0;
@@ -101,8 +123,7 @@ namespace px
 			m_live = nullptr;
 			for (size_t i = 0; i != Size; ++i)
 			{
-				m_pool[i].prev_free = i == 0 ? nullptr : &m_pool[i - 1];
-				m_pool[i].next_free = i + 1 != Size ? nullptr : &m_pool[i + 1];
+				m_pool[i].next_free = i + 1 == Size ? nullptr : &m_pool[i + 1];
 				m_pool[i].prev_live = nullptr;
 				m_pool[i].next_live = nullptr;
 			}
@@ -111,13 +132,14 @@ namespace px
 	public:
 		pool() noexcept
 		{
+			static_assert(Size > 0, "px::pool<T, Size> - Size > 0 required"); // common sense and simplifies many formulaes
 			clear();
 		}
 
 	private:
 		std::array<record, Size> m_pool;
-		size_t m_current;
-		record* m_free; // first free node
-		record* m_live; // first living node
+		size_t m_current; // cashed number of living objest for fast size queries
+		record* m_free; // first free node (root)
+		record* m_live; // first living node (root)
 	};
 }
--- tests/src/pool_tests.cpp
+++ tests/src/pool_tests.cpp
@@ -4,32 +4,55 @@
 
 #include <px/common/pool.hpp>
 
-using namespace px;
+#include <list>
 
 TEST_CASE("pool", "[pool]")
 {
-	pool<int, 10> p;
+	auto is_sequental = [](auto& pool) {
+		char* last = nullptr;
+		bool flag = true;
+		pool.enumerate_active([&last, &flag](auto &element) {
+			char* ptr = static_cast<char*>(&element)
+				if (ptr <= last) flag = false; // next in memory
+			last = ptr;
+		});
+		return flag;
+	};
+
+	auto count = [](auto& pool) {
+		size_t counter = 0;
+		pool.enumerate_active([&counter](auto) { ++counter; });
+		return counter;
+	};
+
+	typedef int element;
+	const size_t maximum = 100;
+
+	px::pool<element, maximum> p;
 	REQUIRE(p.size() == 0);
+	REQUIRE(count(p) == 0);
+	REQUIRE(p.empty() == true);
 
-	int counter;
-
-	counter = 0;
-	p.enumerate_active([&counter](auto) { ++counter; });
-	REQUIRE(counter == 0);
-
-	int* zero = p.request();
-	REQUIRE(p.size() == 1);
+	element* zero = p.request();
 	REQUIRE(zero != nullptr);
-
-	counter = 0;
-	p.enumerate_active([&counter](auto) { ++counter; });
-	REQUIRE(counter == 1);
-
+	REQUIRE(p.size() == 1);
+	REQUIRE(count(p) == 1);
+	REQUIRE(p.empty() == false);
 
 	p.release(zero);
 	REQUIRE(p.size() == 0);
+	REQUIRE(count(p) == 0);
+	REQUIRE(p.empty() == true);
 
-	counter = 0;
-	p.enumerate_active([&counter](auto) { ++counter; });
-	REQUIRE(counter == 0);
+	std::list<element*> list;
+
+	for (int i = 0; i < maximum; ++i)
+	{
+		element* e = p.request();
+		REQUIRE(e != nullptr);
+		list.push_back(e);
+	}
+	REQUIRE(p.size() == maximum);
+	REQUIRE(count(p) == maximum);
+	REQUIRE(p.full() == true);
 }
